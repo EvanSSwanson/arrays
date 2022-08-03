bd0ff2690052a3eedc35195ef6f4cd03db6bded9
Evan Swanson <evan.sundelius@gmail.com>
2022-08-03 11:41:00 -0500

    Complete assignment

--- arrays.js
+++ arrays.js
@@ -1,3 +1,16 @@
 var cupPicks = ["Brazil", "France", "England", "Argentina"]
 var titles = [5, 2, 1, 2]
 var european = [false, true, true, false]
+
+// This will add <"Germany"> to the end of my cupPicks array
+cupPicks.push("Germany")
+
+// This will remove the last element, <2>, from my titles array
+titles.pop()
+
+// This will add a <false> boolean to the beginning of my european array
+european.unshift(false)
+
+// Index positions follow the coding convention of starting a count with 0 instead of 1
+// Index position 3 of cupPicks is Argentina
+// Index position 0 of titles is "5"
